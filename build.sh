7213169875005ccd92d8997240f4afa9337cf3a2
Jefino 1537 <jefinojacob1537@gmail.com>
2023-12-15 23:14:23 +0530

    super

--- build.sh
+++ build.sh
@@ -67,44 +67,11 @@ for i in vendor product system system_ext odm_dlkm odm mi_ext vendor_dlkm; do
     mv "$GITHUB_WORKSPACE/${device}/images/$i.img" "$GITHUB_WORKSPACE/super_maker/"
 done
 # Define the path to the directory containing the image files
-image_directory="$GITHUB_WORKSPACE/super_maker/"
+eval "$i"_size="$(du -sb "$GITHUB_WORKSPACE"/super_maker/$i.img | awk "echo \${1}")"
 
-# Define the list of image files
-image_files=("mi_ext.img" "odm.img" "product.img" "system.img" "system_ext.img" "vendor.img" "vendor_dlkm.img")
-
-partition_sizes=""
-
-# Loop through the list of image files
-for image_file in "${image_files[@]}"; do
-    # Get the full path to the image file
-    image_path="$image_directory/$image_file"
-
-    # Check if the file exists
-    if [ -e "$image_path" ]; then
-        # Get the size of the image file
-        image_size=$(du -b "$image_path" | awk '{print $1}')
-
-        # Append the partition configuration to the command
-        partition_sizes+="--partition ${image_file%.*}_a:readonly:${image_size}:qti_dynamic_partitions_a --image ${image_file%.*}_a=${image_path} "
-        partition_sizes+="--partition ${image_file%.*}_b:readonly:0:qti_dynamic_partitions_b "
-    else
-        echo "File not found: $image_path"
-    fi
-done
-
-# Run lpmake command with dynamic sizes
 Start_Time
-"$GITHUB_WORKSPACE"/tools/lpmake \
-  --metadata-size 65536 --super-name super --block-size 4096 \
-  $partition_sizes \
-  --device super \
-  --metadata-slots 3 --group qti_dynamic_partitions_a --group qti_dynamic_partitions_b \
-  --virtual-ab -F \
-  --output "$GITHUB_WORKSPACE/super_maker/super.img"
-
-for i in mi_ext odm product system system_ext vendor vendor_dlkm; do
-  rm -rf "$GITHUB_WORKSPACE/super_maker/$i.img"
-done
+"$GITHUB_WORKSPACE"/tools/lpmake --metadata-size 65536 --super-name super --block-size 4096 --partition mi_ext_a:readonly:"$mi_ext_size":qti_dynamic_partitions_a --image mi_ext_a="$GITHUB_WORKSPACE"/super_maker/mi_ext.img --partition mi_ext_b:readonly:0:qti_dynamic_partitions_b --partition odm_a:readonly:"$odm_size":qti_dynamic_partitions_a --image odm_a="$GITHUB_WORKSPACE"/super_maker/odm.img --partition odm_b:readonly:0:qti_dynamic_partitions_b --partition product_a:readonly:"$product_size":qti_dynamic_partitions_a --image product_a="$GITHUB_WORKSPACE"/super_maker/product.img --partition product_b:readonly:0:qti_dynamic_partitions_b --partition system_a:readonly:"$system_size":qti_dynamic_partitions_a --image system_a="$GITHUB_WORKSPACE"/super_maker/system.img --partition system_b:readonly:0:qti_dynamic_partitions_b --partition system_ext_a:readonly:"$system_ext_size":qti_dynamic_partitions_a --image system_ext_a="$GITHUB_WORKSPACE"/super_maker/system_ext.img --partition system_ext_b:readonly:0:qti_dynamic_partitions_b --partition vendor_a:readonly:"$vendor_size":qti_dynamic_partitions_a --image vendor_a="$GITHUB_WORKSPACE"/super_maker/vendor.img --partition vendor_b:readonly:0:qti_dynamic_partitions_b --partition vendor_dlkm_a:readonly:"$vendor_dlkm_size":qti_dynamic_partitions_a --image vendor_dlkm_a="$GITHUB_WORKSPACE"/super_maker/vendor_dlkm.img --partition vendor_dlkm_b:readonly:0:qti_dynamic_partitions_b --device super:9663676416 --metadata-slots 3 --group qti_dynamic_partitions_a:9663676416 --group qti_dynamic_partitions_b:9663676416 --virtual-ab -F --output "$GITHUB_WORKSPACE"/super_maker/super.img
+End_Time super
 
 mv "$GITHUB_WORKSPACE/super_maker/super.img" "$GITHUB_WORKSPACE/${device}/images/"
 echo moved super
