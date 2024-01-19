44c677af8d54b7eda838929f511e7bac7f65e644
Jefino 1537 <jefinojacob1537@gmail.com>
2024-01-20 00:30:10 +0530

    test

--- aosp.sh
+++ aosp.sh
@@ -81,14 +81,15 @@ super_size=9126805504
 total_size=$((system_size + system_ext_size + product_size + vendor_size + odm_size + odm_dlkm_size + vendor_dlkm_size))
 Start_Time
 # Run lpmake command
-"$GITHUB_WORKSPACE"/tools/lpmake --metadata-size 65536 --super-name super --block-size 4096 --metadata-slots 3 \
-    --partition system_a:readonly:"$system_size":main_a --image system_a=./super_maker/system.img \
-    --partition system_ext_a:readonly:"$system_ext_size":main_a --image system_ext_a=./super_maker/system_ext.img \
-    --partition product_a:readonly:"$product_size":main_a --image product_a=./super_maker/product.img \
-    --partition vendor_a:readonly:"$vendor_size":main_a --image vendor_a=./super_maker/vendor.img \
-    --partition odm_dlkm_a:readonly:"$odm_dlkm_size":main_a --image odm_dlkm_a=./super_maker/odm_dlkm.img \
-    --partition odm_a:readonly:"$odm_size":main_a --image odm_a=./super_maker/odm.img \
-    --partition vendor_dlkm_a:readonly:"$vendor_dlkm_size":main_a --image vendor_dlkm_a=./super_maker/vendor_dlkm.img \
+"$GITHUB_WORKSPACE"/tools/lpmake --metadata-size 65536 --super-name super --block-size 4096 --metadata-slots 2 \
+    --device super:"$super_size" --group main:"$total_size" \
+    --partition system_a:readonly:"$system_size":main --image system_a=./super_maker/system.img \
+    --partition system_ext_a:readonly:"$system_ext_size":main --image system_ext_a=./super_maker/system_ext.img \
+    --partition product_a:readonly:"$product_size":main --image product_a=./super_maker/product.img \
+    --partition vendor_a:readonly:"$vendor_size":main --image vendor_a=./super_maker/vendor.img \
+    --partition odm_dlkm_a:readonly:"$odm_dlkm_size":main --image odm_dlkm_a=./super_maker/odm_dlkm.img \
+    --partition odm_a:readonly:"$odm_size":main --image odm_a=./super_maker/odm.img \
+    --partition vendor_dlkm_a:readonly:"$vendor_dlkm_size":main --image vendor_dlkm_a=./super_maker/vendor_dlkm.img \
     --sparse --output "$GITHUB_WORKSPACE"/super_maker/super.img
 
 End_Time super
