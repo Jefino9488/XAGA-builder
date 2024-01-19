2ddd95233976bc36276e851fd95b102b6148d59f
Jefino 1537 <jefinojacob1537@gmail.com>
2024-01-20 00:12:18 +0530

    test

--- aosp.sh
+++ aosp.sh
@@ -16,7 +16,7 @@ End_Time() {
   ns=${time:0-9}
   s=${time%$ns}
   if [[ $s -ge 10800 ]]; then
-    echo -e "\e[1;34m - This time $1 took: less than 100 milliseconds \e[0m"
+    echo -e "\e[1;34m - Time $1 took: less than 100 milliseconds \e[0m"
   elif [[ $s -ge 3600 ]]; then
     ms=$(expr $ns / 1000000)
     h=$(expr $s / 3600)
@@ -30,13 +30,13 @@ End_Time() {
     ms=$(expr $ns / 1000000)
     min=$(expr $s / 60)
     s=$(expr $s % 60)
-    echo -e "\e[1;34m - This time $1 took: $min minutes $s seconds $ms milliseconds \e[0m"
+    echo -e "\e[1;34m - Time $1 took: $min minutes $s seconds $ms milliseconds \e[0m"
   elif [[ -n $s ]]; then
     ms=$(expr $ns / 1000000)
-    echo -e "\e[1;34m - This time $1 took: $s seconds $ms milliseconds \e[0m"
+    echo -e "\e[1;34m - Time $1 took: $s seconds $ms milliseconds \e[0m"
   else
     ms=$(expr $ns / 1000000)
-    echo -e "\e[1;34m - This time $1 took: $ms milliseconds \e[0m"
+    echo -e "\e[1;34m - Time $1 took: $ms milliseconds \e[0m"
   fi
 }
 
@@ -71,15 +71,14 @@ for i in vendor product system system_ext odm_dlkm odm vendor_dlkm; do
     eval "${i}_size=\$(du -sb \"$GITHUB_WORKSPACE/super_maker/$i.img\" | awk {'print \$1'})"
 done
 
+zip_file="$GITHUB_WORKSPACE/tools/fw.zip"
+extract_folder="$GITHUB_WORKSPACE/${device}/images"
 
-for i in system_b system_ext_b product_b vendor_b odm_dlkm_b vendor_dlkm_b odm_b; do
-    file_path="$GITHUB_WORKSPACE/super_maker/${i}.img"
+# Ensure the extraction folder exists
+mkdir -p "$extract_folder"
 
-    if [ ! -e "$file_path" ]; then
-        echo "Creating $i.img with zero size"
-        touch "$file_path"
-    fi
-done
+# Extract the contents of the zip file to the destination folder
+unzip -q "$zip_file" -d "$extract_folder"
 
 super_size=9126805504
 # Calculate total size of all images
@@ -87,21 +86,13 @@ total_size=$((system_size + system_ext_size + product_size + vendor_size + odm_s
 Start_Time
 # Run lpmake command
 "$GITHUB_WORKSPACE"/tools/lpmake --metadata-size 65536 --super-name super --block-size 4096 --metadata-slots 3 \
-    --device super:"$super_size" --group main_a:"$total_size" --group main_b:"$total_size" --virtual-ab\
     --partition system_a:readonly:"$system_size":main_a --image system_a=./super_maker/system.img \
-    --partition system_b:readonly:0:main_b --image system_b=./super_maker/system_b.img \
     --partition system_ext_a:readonly:"$system_ext_size":main_a --image system_ext_a=./super_maker/system_ext.img \
-    --partition system_ext_b:readonly:0:main_b --image system_ext_b=./super_maker/system_ext_b.img \
     --partition product_a:readonly:"$product_size":main_a --image product_a=./super_maker/product.img \
-    --partition product_b:readonly:0:main_b --image product_b=./super_maker/product_b.img \
     --partition vendor_a:readonly:"$vendor_size":main_a --image vendor_a=./super_maker/vendor.img \
-    --partition vendor_b:readonly:0:main_b --image vendor_b=./super_maker/vendor_b.img \
     --partition odm_dlkm_a:readonly:"$odm_dlkm_size":main_a --image odm_dlkm_a=./super_maker/odm_dlkm.img \
-    --partition odm_dlkm_b:readonly:0:main_b --image odm_dlkm_b=./super_maker/odm_dlkm_b.img \
     --partition odm_a:readonly:"$odm_size":main_a --image odm_a=./super_maker/odm.img \
-    --partition odm_b:readonly:0:main_b --image odm_b=./super_maker/odm_b.img \
     --partition vendor_dlkm_a:readonly:"$vendor_dlkm_size":main_a --image vendor_dlkm_a=./super_maker/vendor_dlkm.img \
-    --partition vendor_dlkm_b:readonly:0:main_b --image vendor_dlkm_b=./super_maker/vendor_dlkm_b.img \
     --sparse --output "$GITHUB_WORKSPACE"/super_maker/super.img
 
 End_Time super
