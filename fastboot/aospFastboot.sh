f1dc0b69d21894c2e1330a282930f23d1099d17e
Jefino9488 <jefinojacob9488@gmail.com>
2024-04-20 00:52:58 +0530

    echo

--- fastboot/aospFastboot.sh
+++ fastboot/aospFastboot.sh
@@ -3,39 +3,57 @@ GITHUB_WORKSPACE="$2"
 device="$3"
 
 magiskPatch="$GITHUB_WORKSPACE"/magisk/boot_patch.sh
-
+Red='\033[1;31m'
+Yellow='\033[1;33m'
+Blue='\033[1;34m'
+Green='\033[1;32m'
 ### System package download
-echo -e "\e[1;31m - Start downloading package \e[0m"
+echo -e "${Blue}- Starting downloading recovery rom"
 aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$GITHUB_WORKSPACE" -o "recovery_rom.zip" "${URL}"
-echo -e "\e[1;31m - Downloaded recovery rom \e[0m"
+echo -e "${Green}- Downloaded recovery rom"
 
 sudo chmod -R 777 "$GITHUB_WORKSPACE/tools"
 mkdir -p "$GITHUB_WORKSPACE/${device}"
 mkdir -p "$GITHUB_WORKSPACE/super_maker/config"
 mkdir -p "$GITHUB_WORKSPACE/zip"
 
+echo -e "${Yellow}- extracting payload.bin"
 RECOVERY_ZIP="recovery_rom.zip"
 7z x "$GITHUB_WORKSPACE/$RECOVERY_ZIP" -o"$GITHUB_WORKSPACE/${device}" payload.bin
 rm -rf "${GITHUB_WORKSPACE:?}/$RECOVERY_ZIP"
+echo -e "${Blue}- extracted payload.bin"
 
 ### in xaga folder
+echo -e "${Yellow}- extracting images"
 mkdir -p "$GITHUB_WORKSPACE/${device}/images"
 "$GITHUB_WORKSPACE/tools/payload-dumper-go" -o "$GITHUB_WORKSPACE/${device}/images" "$GITHUB_WORKSPACE/${device}/payload.bin" >/dev/null
 sudo rm -rf "$GITHUB_WORKSPACE/${device}/payload.bin"
+echo -e "${Blue}- extracted images"
 
 # Move images to the super_maker directory
+echo -e "${Yellow}- moving images to super_maker"
 for i in vendor product system system_ext odm_dlkm odm vendor_dlkm; do
     mv "$GITHUB_WORKSPACE/${device}/images/$i.img" "$GITHUB_WORKSPACE/super_maker/"
-
-    # Define the path to the directory containing the image files
     eval "${i}_size=\$(du -sb \"$GITHUB_WORKSPACE/super_maker/$i.img\" | awk {'print \$1'})"
+    echo -e "${Blue}- moved $i"
 done
 
-super_size=9126805504
 # Calculate total size of all images
+echo -e "${Yellow}- calculating total size of all images"
+super_size=9126805504
 total_size=$((system_size + system_ext_size + product_size + vendor_size + odm_size + odm_dlkm_size + vendor_dlkm_size))
+echo -e "${Blue}- size of all images"
+echo -e "system: $system_size"
+echo -e "system_ext: $system_ext_size"
+echo -e "product: $product_size"
+echo -e "vendor: $vendor_size"
+echo -e "odm: $odm_size"
+echo -e "odm_dlkm: $odm_dlkm_size"
+echo -e "vendor_dlkm: $vendor_dlkm_size"
+echo -e "total size: $total_size"
 
-# Run lpmake command
+# lpmake command to create super image
+echo -e "${Yellow}- creating super image"
 "$GITHUB_WORKSPACE"/tools/lpmake --metadata-size 65536 --super-name super --block-size 4096 --metadata-slots 3 \
     --device super:"$super_size" --group main_a:"$total_size" --group main_b:"$total_size" \
     --partition system_a:readonly:"$system_size":main_a --image system_a=./super_maker/system.img \
@@ -53,26 +71,39 @@ total_size=$((system_size + system_ext_size + product_size + vendor_size + odm_s
     --partition vendor_dlkm_a:readonly:"$vendor_dlkm_size":main_a --image vendor_dlkm_a=./super_maker/vendor_dlkm.img \
     --partition vendor_dlkm_b:readonly:0:main_b \
     --virtual-ab --sparse --output "$GITHUB_WORKSPACE"/super_maker/super.img
-echo -e "\e[1;31m - Super image created successfully. \e[0m"
+
 
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to create super image."
+    echo -e "${Red}- failed to create super image"
     exit 1
 fi
-echo "Super image created successfully."
+echo -e "${Blue}- created super image"
 
+# Move super image to the images directory
+echo -e "${Yellow}- moving super image"
 mv "$GITHUB_WORKSPACE/super_maker/super.img" "$GITHUB_WORKSPACE/${device}/images/"
-echo moved super
+if [ $? -ne 0 ]; then
+    echo -e "${Red}- failed to move super image"
+    exit 1
+fi
+echo -e "${Blue}- moved super image"
 
+echo -e "${Yellow}- ${device} fastboot working directory"
 mkdir -p "$GITHUB_WORKSPACE/${device}/boot"
 mkdir -p "$GITHUB_WORKSPACE/${device}/vendor_boot"
 mkdir -p "$GITHUB_WORKSPACE/zip"
 
+echo -e "${Yellow}- patching boot image"
 cp "$GITHUB_WORKSPACE/${device}/images/boot.img" "$GITHUB_WORKSPACE/${device}/boot/"
 
 chmod -R +x "$GITHUB_WORKSPACE/magisk"
 
 $magiskPatch "$GITHUB_WORKSPACE/${device}/boot/boot.img"
+if [ $? -ne 0 ]; then
+    echo -e "${Red}- failed to patch boot image"
+    exit 1
+fi
+echo -e "${Blue}- patched boot image"
 
 mv "$GITHUB_WORKSPACE/magisk/new-boot.img" "$GITHUB_WORKSPACE/${device}/boot/magisk_boot.img"
 
@@ -83,9 +114,13 @@ mv "$GITHUB_WORKSPACE/${device}/images/vendor_boot.img" "$GITHUB_WORKSPACE/${dev
 mv "$GITHUB_WORKSPACE/tools/flasher.exe" "$GITHUB_WORKSPACE/${device}/"
 
 cd "$GITHUB_WORKSPACE" || exit
+echo -e "${Blue}- created ${device} working directory"
+
+echo -e "${Yellow}- ziping fastboot files"
+
 zip -r "$GITHUB_WORKSPACE/zip/${device}_fastboot.zip" "${device}"
 
-echo "Created ${device}_fastboot.zip"
+echo -e "${Green}- ${device}_fastboot.zip created successfully"
 
 
 
