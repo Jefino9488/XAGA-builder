a29d07f42a219cb4bbf62fb30f216bf2deed1d1b
Jefino <jefinojacob9488@gmail.com>
2024-05-10 08:58:14 +0530

    improve

--- magisk/boot_patch.sh
+++ magisk/boot_patch.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 #######################################################################################
 # Magisk Boot Image Patcher
 #######################################################################################
@@ -32,10 +33,10 @@ getdir() {
   case "$1" in
     */*)
       dir=${1%/*}
-      if [ -z $dir ]; then
+      if [ -z "$dir" ]; then
         echo "/"
       else
-        echo $dir
+        echo "$dir"
       fi
     ;;
     *) echo "." ;;
@@ -55,7 +56,7 @@ abort() {
 # Initialization
 #################
 
-if [ -z $SOURCEDMODE ]; then
+if [ -z "$SOURCEDMODE" ]; then
   # Switch to the location of the script file
   cd "$(getdir "${BASH_SOURCE:-$0}")"
 fi
@@ -63,22 +64,32 @@ fi
 BOOTIMAGE="$1"
 [ -e "$BOOTIMAGE" ] || abort "$BOOTIMAGE does not exist!"
 
+# Check for command dependencies
+command -v magiskboot >/dev/null 2>&1 || abort "magiskboot not found!"
+command -v nanddump >/dev/null 2>&1 || abort "nanddump not found!"
+command -v grep_prop >/dev/null 2>&1 || abort "grep_prop not found!"
+
 # Dump image for MTD/NAND character device boot partitions
 if [ -c "$BOOTIMAGE" ]; then
-  nanddump -f boot.img "$BOOTIMAGE"
+  if ! nanddump -f boot.img "$BOOTIMAGE"; then
+    abort "! Failed to dump boot image from device"
+  fi
   BOOTNAND="$BOOTIMAGE"
   BOOTIMAGE=boot.img
 fi
 
+# Check for image format
+if ! file "$BOOTIMAGE" | grep -q "DOS/MBR boot sector"; then
+  abort "! Unsupported/Unknown image format"
+fi
+
 # Flags
-[ -z $KEEPVERITY ] && KEEPVERITY=false
-[ -z $KEEPFORCEENCRYPT ] && KEEPFORCEENCRYPT=false
-[ -z $PATCHVBMETAFLAG ] && PATCHVBMETAFLAG=false
-[ -z $RECOVERYMODE ] && RECOVERYMODE=false
-[ -z $LEGACYSAR ] && LEGACYSAR=false
-export KEEPVERITY
-export KEEPFORCEENCRYPT
-export PATCHVBMETAFLAG
+: "${KEEPVERITY:=false}"
+: "${KEEPFORCEENCRYPT:=false}"
+: "${PATCHVBMETAFLAG:=false}"
+: "${RECOVERYMODE:=false}"
+: "${LEGACYSAR:=false}"
+export KEEPVERITY KEEPFORCEENCRYPT PATCHVBMETAFLAG RECOVERYMODE LEGACYSAR
 
 chmod -R 755 .
 
@@ -89,21 +100,26 @@ chmod -R 755 .
 CHROMEOS=false
 
 ui_print "- Unpacking boot image"
-./magiskboot unpack "$BOOTIMAGE"
+
+if ! ./magiskboot unpack "$BOOTIMAGE"; then
+  abort "! Failed to unpack boot image"
+fi
 
 case $? in
   0 ) ;;
   1 )
-    abort "! Unsupported/Unknown image format"
-    ;;
-  2 )
     ui_print "- ChromeOS boot image detected"
     CHROMEOS=true
     ;;
   * )
-    abort "! Unable to unpack boot image"
+    abort "! Failed to unpack boot image"
     ;;
 esac
+
+# Check for ramdisk format
+if [ ! -f ramdisk.cpio ]; then
+  abort "! Unsupported/Unknown ramdisk format"
+fi
 
 ###################
 # Ramdisk Restores
@@ -112,7 +128,9 @@ esac
 # Test patch status and do restore
 ui_print "- Checking ramdisk status"
 if [ -e ramdisk.cpio ]; then
-  ./magiskboot cpio ramdisk.cpio test
+  if ! ./magiskboot cpio ramdisk.cpio test; then
+    abort "! Failed to test ramdisk patch status"
+  fi
   STATUS=$?
   SKIP_BACKUP=""
 else
@@ -124,7 +142,6 @@ case $((STATUS & 3)) in
   0 )  # Stock boot
     ui_print "- Stock boot image detected"
     SHA1=$(./magiskboot sha1 "$BOOTIMAGE" 2>/dev/null)
-    cat $BOOTIMAGE > stock_boot.img
     cp -af ramdisk.cpio ramdisk.cpio.orig 2>/dev/null
     ;;
   1 )  # Magisk patched
@@ -169,15 +186,21 @@ SKIP32="#"
 SKIP64="#"
 if [ -f magisk64 ]; then
   PREINITDEVICE=metadata
-  ./magiskboot compress=xz magisk64 magisk64.xz
+  if ! ./magiskboot compress=xz magisk64 magisk64.xz; then
+    abort "! Failed to compress magisk64 binary"
+  fi
   unset SKIP64
 fi
 if [ -f magisk32 ]; then
   PREINITDEVICE=metadata
-  ./magiskboot compress=xz magisk32 magisk32.xz
+  if ! ./magiskboot compress=xz magisk32 magisk32.xz; then
+    abort "! Failed to compress magisk32 binary"
+  fi
   unset SKIP32
 fi
-./magiskboot compress=xz stub.apk stub.xz
+if ! ./magiskboot compress=xz stub.apk stub.xz; then
+  abort "! Failed to compress stub.apk binary"
+fi
 
 echo "KEEPVERITY=$KEEPVERITY" > config
 echo "KEEPFORCEENCRYPT=$KEEPFORCEENCRYPT" >> config
@@ -188,7 +211,7 @@ if [ -n "$PREINITDEVICE" ]; then
 fi
 [ -n "$SHA1" ] && echo "SHA1=$SHA1" >> config
 
-./magiskboot cpio ramdisk.cpio \
+if ! ./magiskboot cpio ramdisk.cpio \
 "add 0750 $INIT magiskinit" \
 "mkdir 0750 overlay.d" \
 "mkdir 0750 overlay.d/sbin" \
@@ -198,8 +221,9 @@ fi
 "patch" \
 "$SKIP_BACKUP backup ramdisk.cpio.orig" \
 "mkdir 000 .backup" \
-"add 000 .backup/.magisk config" \
-|| abort "! Unable to patch ramdisk"
+"add 000 .backup/.magisk config"; then
+  abort "! Failed to patch ramdisk"
+fi
 
 rm -f ramdisk.cpio.orig config magisk*.xz stub.xz
 
@@ -208,13 +232,13 @@ rm -f ramdisk.cpio.orig config magisk*.xz stub.xz
 #################
 
 for dt in dtb kernel_dtb extra; do
-  if [ -f $dt ]; then
-    if ! ./magiskboot dtb $dt test; then
+  if [ -f "$dt" ]; then
+    if ! ./magiskboot dtb "$dt" test; then
       ui_print "! Boot image $dt was patched by old (unsupported) Magisk"
       abort "! Please try again with *unpatched* boot image"
     fi
-    if ./magiskboot dtb $dt patch; then
-      ui_print "- Patch fstab in boot image $dt"
+    if ! ./magiskboot dtb "$dt" patch; then
+      abort "! Failed to patch $dt in boot image"
     fi
   fi
 done
@@ -222,26 +246,35 @@ done
 if [ -f kernel ]; then
   PATCHEDKERNEL=false
   # Remove Samsung RKP
-  ./magiskboot hexpatch kernel \
+  if ! ./magiskboot hexpatch kernel \
   49010054011440B93FA00F71E9000054010840B93FA00F7189000054001840B91FA00F7188010054 \
-  A1020054011440B93FA00F7140020054010840B93FA00F71E0010054001840B91FA00F7181010054 \
-  && PATCHEDKERNEL=true
+  A1020054011440B93FA00F7140020054010840B93FA00F71E0010054001840B91FA00F7181010054; then
+    abort "! Failed to remove Samsung RKP from kernel"
+  fi
+  PATCHEDKERNEL=true
 
   # Remove Samsung defex
   # Before: [mov w2, #-221]   (-__NR_execve)
   # After:  [mov w2, #-32768]
-  ./magiskboot hexpatch kernel 821B8012 E2FF8F12 && PATCHEDKERNEL=true
+  if ! ./magiskboot hexpatch kernel 821B8012 E2FF8F12; then
+    abort "! Failed to remove Samsung defex from kernel"
+  fi
+  PATCHEDKERNEL=true
 
   # Force kernel to load rootfs for legacy SAR devices
   # skip_initramfs -> want_initramfs
-  $LEGACYSAR && ./magiskboot hexpatch kernel \
+  if $LEGACYSAR && ! ./magiskboot hexpatch kernel \
   736B69705F696E697472616D667300 \
-  77616E745F696E697472616D667300 \
-  && PATCHEDKERNEL=true
+  77616E745F696E697472616D667300; then
+    abort "! Failed to force kernel to load rootfs for legacy SAR devices"
+  fi
+  PATCHEDKERNEL=true
 
   # If the kernel doesn't need to be patched at all,
   # keep raw kernel to avoid bootloops on some weird devices
-  $PATCHEDKERNEL || rm -f kernel
+  if ! $PATCHEDKERNEL; then
+    rm -f kernel
+  fi
 fi
 
 #################
@@ -249,13 +282,5 @@ fi
 #################
 
 ui_print "- Repacking boot image"
-./magiskboot repack "$BOOTIMAGE" || abort "! Unable to repack boot image"
 
-# Sign chromeos boot
-$CHROMEOS && sign_chromeos
-
-# Restore the original boot partition path
-[ -e "$BOOTNAND" ] && BOOTIMAGE="$BOOTNAND"
-
-# Reset any error code
-true
+if ! ./magiskboot repack "$BOOTIMAGE
