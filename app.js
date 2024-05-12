97594ae9dee043f919ff058e715e6a9afbe777a1
Jefino9488 <jefinojacob9488@gmail.com>
2024-05-12 11:52:31 +0530

    beta web

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest';
+document.addEventListener('DOMContentLoaded', async () => {
   const form = document.getElementById('fastboot-form');
+  const urlInput = document.getElementById('url-input');
+  const romTypeSelect = document.getElementById('rom-type-select');
+  const nameInput = document.getElementById('name-input');
+
+  const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+  const REPO_OWNER = 'Jefino9488';
+  const REPO_NAME = 'XAGA-builder';
+  const WORKFLOW_ID = 'FASTBOOT.yml';
+
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN
+  });
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const urlInput = document.getElementById('url-input');
-    const romTypeSelect = document.getElementById('rom-type-select');
-    const nameInput = document.getElementById('name-input');
-    const githubToken = ''; // your GitHub token
-    const repoOwner = 'Jefino9488';
-    const repoName = 'XAGA-builder';
-    const workflowId = 'Fastboot';
-
-    const headers = {
-      'Authorization': `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
-      'Content-Type': 'application/json'
-    };
-
-    const data = {
-      'inputs': {
-        'URL': urlInput.value,
-        'ROM_TYPE': romTypeSelect.value,
-        'Name': nameInput.value
-      }
-    };
-
     try {
-      const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/actions/workflows/${workflowId}/dispatches`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(data)
+      const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+        owner: REPO_OWNER,
+        repo: REPO_NAME,
+        workflow_id: WORKFLOW_ID,
+        ref: 'fastboot', // Add this line
+        inputs: {
+          URL: urlInput.value,
+          ROM_TYPE: romTypeSelect.value,
+          Name: nameInput.value
+        }
       });
 
-      if (response.ok) {
+      if (response.status === 204) {
         console.log('GitHub Action triggered successfully!');
       } else {
         console.error('Error triggering GitHub Action:', response.status);
