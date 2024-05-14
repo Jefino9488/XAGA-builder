4410ea5d5195605b49d6556bd3647cdb1bc91b45
Jefino9488 <jefinojacob9488@gmail.com>
2024-05-14 22:58:34 +0530

    fix merge.bat

--- tools/merge.bat
+++ tools/merge.bat
@@ -5,28 +5,24 @@ set "outputFile=_fastboot.zip"
 set "progress=0"
 set "totalFiles=0"
 
-rem Count total number of files and store their names in an array
-set "files[]="
-for /R %%i in (*_fastboot-split.zip) do (
+rem Count total number of files
+for %%i in (*_fastboot-split.zip*) do (
     set /a totalFiles+=1
-    set "files[!totalFiles!]=%%i"
 )
 
 echo Merging %totalFiles% files into "%outputFile%"
 
-for /L %%i in (1, 1, %totalFiles%) do (
-    set "filename=!files[%%i]!_fastboot"
-    set "filename=!filename:~0,-4!"
+for %%i in (*_fastboot-split.zip*) do (
+    set "filename=%%~ni"
+    set "filename=!filename:_fastboot-split=!"
 
-    echo Merging "!files[%%i]!" into "!filename!.zip"
-    copy /b "!files[%%i]!" "!filename!.zip" + && del "!files[%%i]!"
+    echo Merging "!filename!!outputFile!"
+    type "%%i" >> "!filename!!outputFile!"
 
     set /a progress+=1
     echo Merged !progress! out of %totalFiles% files
 )
 
 echo Merging complete! Output file: %outputFile%
-echo Deleting split zip files
-del /Q *_fastboot-split.zip
 endlocal
 pause
