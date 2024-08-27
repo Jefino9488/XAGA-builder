d586a456ac702e95983bdeacf047e671fe552d0c
Jefino9488 <jefinojacob9488@gmail.com>
2024-08-27 11:14:25 +0530

    fix few typos

--- Builder/test.sh
+++ Builder/test.sh
@@ -1,42 +1,44 @@
-#!/bin/bash
-
-# Define colors
-colors=(
-    "\033[1;31m" # red
-    "\033[1;33m" # yellow
-    "\033[1;34m" # blue
-    "\033[1;32m" # green
-    "\033[0m"   # reset
-)
-
-# Set timezone, remove packages and install required tools
-sudo timedatectl set-timezone Asia/Shanghai
 sudo apt-get remove -y firefox zstd
-sudo apt-get install -y python3 aria2
+sudo apt-get install python3 aria2
 
-# Parse input arguments
-if [ "$#" -ne 4 ]; then
-    echo -e "${colors[0]}Error: Invalid number of arguments. Expected 4, received ${#}.${colors[5]}"
-    exit 1
-fi
+URL="$1"
+DEVICE="$2"
+WORKSPACE="$3"
 
-url="$1"
-vendor_url="$2"
-github_env="$3"
-github_workspace="$4"
+# Download package
+echo -e "${BLUE}- Downloading package"
+aria2c -x16 -j"$(nproc)" -U "Mozilla/5.0" -d "${WORKSPACE}" -o "recovery_rom.zip" "${URL}"
 
-# Validate URLs
-if [[ ! "$url" =~ ^https?:// ]]; then
-    echo -e "${colors[0]}Error: Invalid URL format for 'url'.${colors[5]}"
-    exit 1
-fi
+# Extract payload.bin
+echo -e "${YELLOW}- extracting payload.bin"
+recovery_zip="recovery_rom.zip"
+7z x "${WORKSPACE}/${recovery_zip}" -o"${WORKSPACE}/${DEVICE}" payload.bin || true
+rm -rf "${WORKSPACE:?}/${recovery_zip}"
+echo -e "${BLUE}- extracted payload.bin"
 
-if [[ ! "$vendor_url" =~ ^https?:// ]]; then
-    echo -e "${colors[0]}Error: Invalid URL format for 'vendor_url'.${colors[5]}"
-    exit 1
-fi
+# Extract images
+echo -e "${YELLOW}- extracting images"
+mkdir -p "${WORKSPACE}/${DEVICE}/images"
+"${WORKSPACE}/tools/payload-dumper-go" -o "${WORKSPACE}/${DEVICE}/images" "${WORKSPACE}/${DEVICE}/payload.bin" >/dev/null
+sudo rm -rf "${WORKSPACE}/${DEVICE}/payload.bin"
+echo -e "${BLUE}- extracted images"
 
-echo -e "URL: ${colors[3]}$url${colors[5]}"
-echo -e "Vendor URL: ${colors[3]}$vendor_url${colors[5]}"
-echo -e "GitHub Environment: ${colors[3]}$github_env${colors[5]}"
-echo -e "GitHub Workspace: ${colors[3]}$github_workspace${colors[5]}"
+# decompress images
+echo -e "${YELLOW}- decompressing images"
+mkdir -p "${WORKSPACE}/${DEVICE}/images/decompressed"
+for img in product system system_ext vendor odm; do
+    "${WORKSPACE}/tools/sdat2img" "${WORKSPACE}/${DEVICE}/images/$img.img" "${WORKSPACE}/${DEVICE}/images/decompressed/$img.img" || exit
+    echo -e "${BLUE}- decompressed $img"
+done
+
+# list all content 
+
+echo -e "${YELLOW}- listing all content"
+ls -alh "${WORKSPACE}/${DEVICE}/images/decompressed"
+ls -alh "${WORKSPACE}/${DEVICE}/images"
+
+# Clean up
+rm -rf "${WORKSPACE:?}/${DEVICE:?}/images"
+rm -rf "${WORKSPACE:?}/${DEVICE:?}/payload.bin"
+rm -rf "${WORKSPACE:?}/${DEVICE:?}/recovery_rom.zip"
+echo -e "${GREEN}- Cleaned up"
