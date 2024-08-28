5d756cafa747ccccdab8c7b94ce6f296495c9b86
Jefino9488 <jefinojacob9488@gmail.com>
2024-08-28 13:12:33 +0530

    remove unwanted

--- Builder/scripts/product.sh
+++ Builder/scripts/product.sh
@@ -1,17 +1,23 @@
-
 DEVICE="$1"
 WORKSPACE="$2"
+
 # modify
 echo -e "${YELLOW}- modifying product"
 ls -alh "${WORKSPACE}/${DEVICE}/images/product/data-app/"
-ls -alh "${WORKSPACE}/${DEVICE}/images/product/data-app/system"
-unwanted_files=("MIUIMiDrive" "MIUIDuokanReader" "MIUIQuickSearchBox" "MIUIHuanji" "MIUIGameCenter" "Health" "MIGalleryLockscreen-MIUI15" "MIMediaEditor" "MIUICalculator" "MIUICleanMaster" "MIUICompass" "MIUIEmail" "MIUINewHome_Removable" "MIUINotes" "MIUIScreenRecorderLite" "MIUISoundRecorderTargetSdk30" "MIUIVipAccount" "MIUIVirtualSim" "MIUIWeather" "MIUIXiaoAiSpeechEngine" "MIUIYoupin" "MiRadio" "MiShop" "MiuiScanner" "SmartHome" "ThirdAppAssistant" "XMRemoteController" "com.iflytek.inputmethod.miui" "wps-lite" "BaiduIME")
+ls -alh "${WORKSPACE}/${DEVICE}/images/product/app/"
+ls -alh "${WORKSPACE}/${DEVICE}/images/product/priv-app/"
+unwanted_files=("MIUIMiDrive" "MIUIDuokanReader" "MIUIQuickSearchBox" "MIUIHuanji" "MIUIGameCenter" "Health" "MIGalleryLockscreen-MIUI15" "MIMediaEditor" "MIUICalculator" "MIUICleanMaster" "MIUICompass" "MIUIEmail" "MIUINewHome_Removable" "MIUINotes" "MIUIScreenRecorderLite" "MIUISoundRecorderTargetSdk30" "MIUIVipAccount" "MIUIVirtualSim" "MIUIXiaoAiSpeechEngine" "MIUIYoupin" "MiRadio" "MiShop" "MiuiScanner" "SmartHome" "ThirdAppAssistant" "XMRemoteController" "com.iflytek.inputmethod.miui" "wps-lite" "BaiduIME" "MiuiDaemon" "MiuiBugReport" "Updater" "MiService" "MiBrowserGlobal" "Music" "XiaomiEUExt" "MiShare" "MiuiVideoGlobal" "GoogleLens" "MiGalleryLockscreen" "MiMover" "PrintSpooler" "CatchLog" "facebook-appmanager" "MIUICompassGlobal" "MIUIHealthGlobal" "MIUIVideoPlayer" "facebook-installer" "facebook-services" "MIShareGlobal" "MIUIMusicGlobal" "MIBrowserGlobal" "MIDrop" "MIUISystemAppUpdater")
 
-for file in "${unwanted_files[@]}"; do
-  appsuite=$(find "${WORKSPACE}/${DEVICE}/images/product/data-app/" -type d -name "*$file")
-  if [ -d "$appsuite" ]; then
-    echo -e "${YELLOW}- removing: $file"
-    sudo rm -rf "$appsuite"
-  fi
+dirs=("images/product/app" "images/product/priv-app" "images/product/data-app")
+
+for dir in "${dirs[@]}"; do
+  for file in "${unwanted_files[@]}"; do
+    appsuite=$(find "${WORKSPACE}/${DEVICE}/${dir}/" -type d -name "*$file")
+    if [ -d "$appsuite" ]; then
+      echo -e "${YELLOW}- removing: $file from $dir"
+      sudo rm -rf "$appsuite"
+    fi
+  done
 done
+
 echo -e "${BLUE}- modified product"
